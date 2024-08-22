df8a0aecf40e91852d261d93cd5abae06a4c36d3
Eslam Eltayar <tayaruwk@gmail.com>
2024-08-22 03:48:15 +0300

    Add Form ViewModel

---
+++ GameZilla.Entities/ViewModels/CreateFormGameViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace GameZilla.Entities.ViewModels
+{
+    public class CreateFormGameViewModel
+    {
+        [MaxLength(250)]
+        public string Name { get; set; } = string.Empty;
+
+        public int CategoryId { get; set; } // FK
+        public IEnumerable<SelectListItem> Categories { get; set; } = Enumerable.Empty<SelectListItem>();
+
+        public List<int> SelectedDevices { get; set; } = new List<int>();
+
+        public IEnumerable<SelectListItem> Devices { get; set; } = Enumerable.Empty<SelectListItem>();
+
+
+        [MaxLength(2500)]
+        public string Description { get; set; } = string.Empty;
+
+        public IFormFile Cover { get; set; } = default!;
+
+    }
+}
